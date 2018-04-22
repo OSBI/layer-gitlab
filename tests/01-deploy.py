259e3161283616ee2039f117a54505f4fe12a252
Tom Barber <tom@analytical-labs.com>
2018-04-22 21:48:51 +0100

    remote charm deploy

--- tests/01-deploy.py
+++ tests/01-deploy.py
@@ -1,43 +1,56 @@
 #!/usr/bin/python3
 
-# import os
+
 import amulet
 import requests
-
-d = amulet.Deployment(series='xenial')
-d.add('gitlab', 'cs:~spiculecharms/gitlab-server')
-d.expose('gitlab')
-
-d.add('haproxy', 'cs:haproxy')
-d.expose('haproxy')
-
-try:
-    d.setup(timeout=1200)
-    d.sentry.wait()
-except amulet.helpers.TimeoutError:
-    amulet.raise_status(amulet.SKIP, msg="Environment wasn't stood up in time")
-except:
-    raise
-
-gitlab_unit = d.sentry['gitlab'][0]
-haproxy_unit = d.sentry['haproxy'][0]
-
-home_page = requests.get('http://%s:80/' % gitlab_unit.info['public-address'])
-home_page.raise_for_status()  # Make sure it's not 5XX error
-assert "GitLab Community Edition" in home_page.text
-
-d.relate('haproxy:reverseproxy', 'gitlab:website')
-
-home_page = requests.get('http://%s:80/' % haproxy_unit.info['public-address'])
-home_page.raise_for_status()
-
-assert "GitLab Community Edition" in home_page.text
+import unittest
 
 
-d.configure('gitlab', {
-    'external_url': 'http://test.spiculecharms.com',
-})
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.d = amulet.Deployment(series='xenial')
+        self.d.add('gitlab', 'cs:~spiculecharms/gitlab-server')
+        self.d.expose('gitlab')
 
-contents = d.sentry['gitlab/0'].file_contents('/etc/gitlab/gitlab.rb')
+        self.d.add('haproxy', 'cs:haproxy')
+        self.d.expose('haproxy')
+    
 
-assert "test.spiculecharms.com" in contents
+        self.d.setup(timeout=1200)
+        self.d.sentry.wait()
+
+        self.gitlab_unit = self.d.sentry['gitlab'][0]
+
+    def test_service(self):
+
+        haproxy_unit = d.sentry['haproxy'][0]
+
+        home_page = requests.get('http://%s:80/' % gitlab_unit.info['public-address'])
+        home_page.raise_for_status()  # Make sure it's not 5XX error
+        assert "GitLab Community Edition" in home_page.text
+
+        d.relate('haproxy:reverseproxy', 'gitlab:website')
+
+        home_page = requests.get('http://%s:80/' % haproxy_unit.info['public-address'])
+        home_page.raise_for_status()
+
+        assert "GitLab Community Edition" in home_page.text
+
+    def test_change_url(self):
+        d.configure('gitlab', {
+            'external_url': 'http://test.spiculecharms.com',
+        })
+
+        contents = d.sentry['gitlab/0'].file_contents('/etc/gitlab/gitlab.rb')
+
+        assert "test.spiculecharms.com" in contents
+
+
+    def test_set_root_password(self):
+
+
+    def test_login(self):
+
+    def test_create_repo(self):
+
+    def test_push_to_repo(self):
